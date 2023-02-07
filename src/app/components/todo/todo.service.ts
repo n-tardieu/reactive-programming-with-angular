0f5750274d7e5217cd145a412852617abcb498c9
Nicolas Tardieu <ntardieu.contact@gmail.com>
2023-02-07 19:44:28 +0100

    feat(todo): start init store

---
+++ src/app/components/todo/todo.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo.model';
+
+@Injectable({ providedIn: 'root' })
+export class TodoService {
+  private storageInitialised = false;
+
+  constructor(private storage: Storage) {}
+
+  async getTodos(): Promise<Todo[]> {
+    if (!this.storageInitialised) await this.storage.create();
+
+    return (await this.storage.get('todos')) || [];
+  }
+
+  async saveTodos(todos: Todo[]) {
+    if (!this.storageInitialised) await this.storage.create();
+
+    return this.storage.set('todos', todos);
+  }
+}
