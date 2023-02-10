80ef9fbdb81f533f89ad3fc26543add80d003490
Nicolas Tardieu <ntardieu.contact@gmail.com>
2023-02-10 18:35:08 +0100

    fix(app.module): clear excess import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoComponent } from './components/todo/todo.component';
 import { TodoEffects } from './state/todos/todo.effects';
 import { todoReducer } from './state/todos/todo.reducer';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
   ],
   imports: [
     BrowserModule,
