402eb6911df161b9c16ec4589e87b51ff113cc4b
Nicolas Tardieu <ntardieu.contact@gmail.com>
2023-02-11 20:29:21 +0100

    fix(todo): add Store<AppState>

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/state/app.state';
 import { addTodo, loadTodos, removeTodo } from '../../state/todos/todo.actions';
 import { selectAllTodos } from '../../state/todos/todo.selector';
 import { Todo } from './todo.model';
@@ -10,11 +12,13 @@ import { Todo } from './todo.model';
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
+  public allTodos$: Observable<Todo[]>;
 
-  public allTodos$ = this.store.select(selectAllTodos);
   public todo = '';
 
-  constructor(private store: Store) { }
+  constructor(private store: Store<AppState>) {
+    this.allTodos$ = this.store.select(selectAllTodos);
+  }
 
   ngOnInit() {
     this.store.dispatch(loadTodos());
