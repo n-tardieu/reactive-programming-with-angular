6ea5cd946a2c9bc1f09eaeb9288a6439954b1e44
Nicolas Tardieu <ntardieu.contact@gmail.com>
2023-02-21 23:31:17 +0100

    feat(moviesSearch): add search feature

--- src/app/movies/features/movies-search/movies-search.component.ts
+++ src/app/movies/features/movies-search/movies-search.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, map, Observable, switchMap, tap } from 'rxjs';
+import { MoviesService } from '../../data-access/movies.service';
 
 @Component({
   selector: 'app-movies-search',
   templateUrl: './movies-search.component.html',
   styleUrls: ['./movies-search.component.scss']
 })
-export class MoviesSearchComponent {
+export class MoviesSearchComponent implements OnInit {
+
+  searchControl = new FormControl();
+  searchResults$: Observable<string[]>;
+
+
+  constructor(private moviesService: MoviesService) {
+    this.searchResults$ = this.searchControl.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(query => this.moviesService.search(query))
+    );
+  }
+
+  ngOnInit(): void { }
 
 }
