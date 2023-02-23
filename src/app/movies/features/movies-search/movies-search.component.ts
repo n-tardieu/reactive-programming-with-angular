4600fbee198119150c86cab3935631de76fcb019
Nicolas Tardieu <ntardieu.contact@gmail.com>
2023-02-23 21:05:11 +0100

    feat(moviesSearch): update #loading #moviesNotFound

--- src/app/movies/features/movies-search/movies-search.component.ts
+++ src/app/movies/features/movies-search/movies-search.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, map, Observable, switchMap, tap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, Observable, of, startWith, switchMap, tap } from 'rxjs';
 import { MoviesService } from '../../data-access/movies.service';
 
 @Component({
@@ -12,17 +12,21 @@ import { MoviesService } from '../../data-access/movies.service';
 export class MoviesSearchComponent implements OnInit {
 
   searchControl = new FormControl();
-  searchResults$: Observable<string[]>;
-
+  searchResults$: Observable<string[]> = of([]);
 
   constructor(private moviesService: MoviesService) {
     this.searchResults$ = this.searchControl.valueChanges.pipe(
+      startWith(''),
       debounceTime(300),
       distinctUntilChanged(),
-      switchMap(query => this.moviesService.search(query))
+      switchMap(query => this.moviesService.search(query)),
     );
   }
 
   ngOnInit(): void { }
+
+  public trackByMovie(i: number, _item: string): number {
+    return i;
+  }
 
 }
